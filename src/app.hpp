bfbc7b4985911903ccac5c88a05425a33204f772
Calvin Bonomo <cal.bonomo@gmail.com>
2025-11-03 07:42:17 -0700

    Added swap chain construction

--- src/app.hpp
+++ src/app.hpp
@@ -11,6 +11,9 @@
 #include <cstring>
 #include <optional>
 #include <set>
+#include <cstdint>
+#include <limits>
+#include <algorithm>
 
 struct QueueFamilyIndices 
 {
@@ -21,6 +24,13 @@ struct QueueFamilyIndices
     {
         return graphicsFamily.has_value() && presentFamily.has_value();
     }
+};
+
+struct SwapChainSupportDetails
+{
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
 };
 
 class HelloTriangleApplication
@@ -48,10 +58,16 @@ private:
 
     void PickPhysicalDevice();
     bool IsDeviceSuitable(VkPhysicalDevice device);
+    bool CheckDeviceExtensionSupport(VkPhysicalDevice device);
     QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
     void CreateLogicalDevice();
 
     void CreateSurface();
+    SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device);
+    VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats);
+    VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR> &availablePresentModes);
+    VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
+    void CreateSwapChain();
 
 private:
     GLFWwindow *m_Window;
@@ -64,12 +80,22 @@ private:
 
     VkSurfaceKHR m_Surface;
     VkQueue m_PresentQueue;
+
+    VkSwapchainKHR m_Swapchain;
+
+    std::vector<VkImage> m_SwapchainImages;
+    VkFormat m_SwapchainImageFormat;
+    VkExtent2D m_SwapchainExtent;
     
     static constexpr uint32_t WINDOW_WIDTH = 800;
     static constexpr uint32_t WINDOW_HEIGHT = 600;
 
     const std::vector<const char *> m_ValidationLayers = {
         "VK_LAYER_KHRONOS_validation",
+    };
+
+    const std::vector<const char *> m_DeviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
     };
 
 #if NDEBUG
