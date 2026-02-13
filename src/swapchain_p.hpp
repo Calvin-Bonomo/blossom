da48b7d279c54aeecdec7dc69ca861e82add99a1
Calvin Bonomo <cal.bonomo@gmail.com>
2026-02-13 08:27:52 -0700

    made swapchain header private

--- src/swapchain_p.hpp
+++ src/swapchain_p.hpp
@@ -6,6 +6,7 @@ namespace blossom
 {
     struct Swapchain 
     {
+        vk::SurfaceKHR m_WindowSurface;
         vk::SwapchainKHR m_Swapchain;
         // color attachment color format
         // depth stencil attachment format
